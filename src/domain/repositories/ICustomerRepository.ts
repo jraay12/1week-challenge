9d751e57685236453a8d98a4160c2511dba3bb64
John Ray <johnraycaete@gmail.com>
2026-03-07 14:39:30 +0800

    feat: add findById method to CustomerRepository

--- src/domain/repositories/ICustomerRepository.ts
+++ src/domain/repositories/ICustomerRepository.ts
@@ -3,4 +3,5 @@ import { Customer } from "../entities/customer.entity";
 export interface ICustomerRepository {
   register(customer: Customer): Promise<void>;
   findByEmail(email: string): Promise<Customer | null>;
+  findById(customer_id: string): Promise<Customer | null>
 }
