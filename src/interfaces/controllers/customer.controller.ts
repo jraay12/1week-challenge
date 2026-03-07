5c23d0b156edec719ce6638617ddf2ce2bef5ed1
John Ray <johnraycaete@gmail.com>
2026-03-07 13:56:15 +0800

    feat: add login endpoint in CustomerController

--- src/interfaces/controllers/customer.controller.ts
+++ src/interfaces/controllers/customer.controller.ts
@@ -2,9 +2,13 @@ import { FastifyRequest, FastifyReply } from "fastify";
 import { CreateCustomerUsecase } from "../../application/usecases/CreateCustomerUsecase";
 import { CreateCustomerDTO } from "../../application/dto/CreateCustomerDTO";
 import { CustomerResponseDTO } from "../../application/dto/CustomerResponseDTO";
-
+import { LoginUsecase } from "../../application/usecases/LoginUsecase";
+import { LoginDTO } from "../../application/dto/LoginDTO";
 export class CustomerController {
-  constructor(private createCustomerUsecase: CreateCustomerUsecase) {}
+  constructor(
+    private createCustomerUsecase: CreateCustomerUsecase,
+    private loginUsecase: LoginUsecase,
+  ) {}
 
   create = async (
     request: FastifyRequest,
@@ -13,6 +17,18 @@ export class CustomerController {
     const input = request.body as CreateCustomerDTO;
     const customer: CustomerResponseDTO =
       await this.createCustomerUsecase.execute(input);
-    reply.status(201).send({message: "customer successfully registered", result: customer});
+    reply
+      .status(201)
+      .send({ message: "customer successfully registered", result: customer });
+  };
+
+  login = async (
+    request: FastifyRequest,
+    reply: FastifyReply,
+  ): Promise<void> => {
+    const input = request.body as LoginDTO;
+    const { accessToken, refreshToken } =
+      await this.loginUsecase.execute(input);
+    reply.status(200).send({ accessToken });
   };
 }
