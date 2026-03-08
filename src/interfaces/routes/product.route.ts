3793ce1959ed96cb816b65bb62a86d22e82f5a5b
John Ray <johnraycaete@gmail.com>
2026-03-08 14:38:30 +0800

    feat: add authenticate middleware for the sale & product route

--- src/interfaces/routes/product.route.ts
+++ src/interfaces/routes/product.route.ts
@@ -5,8 +5,10 @@ export async function productRoutes(fastify: FastifyInstance) {
     fastify.productController.create(req, reply),
   );
 
-  fastify.get("/all-product", async (req, reply) =>
-    fastify.productController.getAll(req, reply),
+  fastify.get(
+    "/all-product",
+    { preHandler: [fastify.authenticationMiddleware.authenticate] },
+    (req, reply) => fastify.productController.getAll(req, reply),
   );
 
   fastify.patch("/add-stock/:productId", async (req, reply) =>
--- src/interfaces/routes/sales.route.ts
+++ src/interfaces/routes/sales.route.ts
@@ -1,8 +1,10 @@
 import { FastifyInstance } from "fastify";
 
 export async function salesRoutes(fastify: FastifyInstance) {
-  fastify.post("/create/:productId", async (req, reply) =>
-    fastify.salesController.create(req, reply),
+  fastify.post(
+    "/create/:productId",
+    { preHandler: [fastify.authenticationMiddleware.authenticate] },
+    (req, reply) => fastify.salesController.create(req, reply),
   );
 
   fastify.get("/month", async (req, reply) =>
