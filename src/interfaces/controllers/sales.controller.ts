7ea449c3fa03f827e1e8c6ccb07c1931796ee2b7
John Ray <johnraycaete@gmail.com>
2026-03-08 14:18:49 +0800

    feat: add getSalesByMonth endpoint in SalesController

--- src/interfaces/controllers/sales.controller.ts
+++ src/interfaces/controllers/sales.controller.ts
@@ -1,9 +1,11 @@
 import { CreateSaleDTO } from "../../application/dto/CreateSaleDTO";
 import { CreateSalesUsecase } from "../../application/usecases/CreateSaleUsecase";
 import { FastifyRequest, FastifyReply } from "fastify";
+import { GetSalesMonthUsecase } from "../../application/usecases/GetSalesByMonthUsecase";
+import { MonthlySalesDTO } from "../../application/dto/MonthlySalesDTO";
 
 export class SalesController {
-  constructor(private createSalesUsecase: CreateSalesUsecase) {}
+  constructor(private createSalesUsecase: CreateSalesUsecase, private getMonthlySalesUsecase: GetSalesMonthUsecase) {}
 
   create = async (request: FastifyRequest, reply: FastifyReply) => {
     const { productId } = request.params as { productId: string };
@@ -21,6 +23,15 @@ export class SalesController {
     reply.status(201).send({
       message: "Sale created successfully",
       result: sale,
+    });
+  };
+
+  getSalesByMonth = async (request: FastifyRequest, reply: FastifyReply) => {
+    const { month, year } = request.query as { month: number; year: number };
+    const sales: MonthlySalesDTO[] = await this.getMonthlySalesUsecase.execute(month, year);
+    reply.status(200).send({
+      message: `Sales for ${month}/${year}`,
+      result: sales,
     });
   };
 }
