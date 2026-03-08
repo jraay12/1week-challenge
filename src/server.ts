13c8b01e7a6ab88170808b832406d7e5fc34fec1
John Ray <johnraycaete@gmail.com>
2026-03-08 17:43:03 +0800

    feat: make Fastify listen on all interfaces (0.0.0.0) for Docker

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import fastify from "./app";
 
 const PORT = Number(process.env.PORT) || 3000;
 
-fastify.listen({ port: PORT }, function (err, address) {
+fastify.listen({ port: PORT,  host: "0.0.0.0" }, function (err, address) {
   if (err) {
     fastify.log.error(err);
     process.exit(1);
